d5773cd0d938097d63a8d63a7d0d35a6d830c899
Tyler Grace <tylerjamesgrace@gmail.com>
2026-09-11 13:35:09 -0500

    Added Hashmaps

---
+++ hashmaps/hashfunction.go
@@ -0,0 +1,26 @@
+package main
+
+func FNV1aHash(key string) uint32 {
+	var hash uint32 = 2166136261 //offset basis, fixed constant
+	for i := 0; i < len(key); i++ {
+		hash ^= uint32(key[i])
+		hash *= 16777619
+	}
+	return hash
+}
+
+func djb2(key string) uint32 {
+	var hash uint32 = 5381
+	for i := 0; i < len(key); i++ {
+		hash = hash*33 + uint32(key[i])
+	}
+	return hash
+}
+
+func djb2a(key string) uint32 {
+	var hash uint32 = 5381
+	for i := 0; i < len(key); i++ {
+		hash = hash*33 ^ uint32(key[i])
+	}
+	return hash
+}
